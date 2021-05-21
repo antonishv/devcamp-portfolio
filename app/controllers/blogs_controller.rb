b2b379186706ce7b21dec9e1a64fab04a6e1fe33
antonishv <antonishv@gmail.com>
2021-05-21 14:37:09 +0700

    Added navbar and added default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,10 +4,13 @@ class BlogsController < ApplicationController
   # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1 or /blogs/1.json
   def show
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
@@ -17,6 +20,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs/1/edit
   def edit
+    @page_title = "Edit #{@blog.title}"
   end
 
   # POST /blogs or /blogs.json
