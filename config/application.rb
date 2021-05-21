52b181e84f1d343044df0d60551ecda153ac35fa
antonishv <antonishv@gmail.com>
2021-05-21 13:46:07 +0700

    Edit guest user for current user

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module DevcampPortfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # For allow all params
+    # config.action_controller.permit_all_parameters = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
